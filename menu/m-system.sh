4b4798da4649bfc64e5b00e24e23a84c15c31980
ngacaprak0809-commits <ngacaprak0809@gmail.com>
2026-02-16 11:50:40 +0700

    Update m-system.sh

--- menu/m-system.sh
+++ menu/m-system.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\E[0;100;33m          • SYSTEM MENU •          \E[0m"
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\\033[0m"
+echo -e "\\E[0;100;33m          • SYSTEM MENU •          \\E[0m"
+echo -e "\\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\\033[0m"
 echo -e ""
-echo -e " [\e[36m•1\e[0m] Panel Domain"
-echo -e " [\e[36m•2\e[0m] Speedtest VPS"
-echo -e " [\e[36m•3\e[0m] Set Auto Reboot"
-echo -e " [\e[36m•4\e[0m] Restart All Service"
-echo -e " [\e[36m•5\e[0m] Cek Bandwith"
-echo -e " [\e[36m•6\e[0m] Install TCP BBR"
-echo -e " [\e[36m•7\e[0m] DNS CHANGER"
-echo -e " [\e[36m•8\e[0m] Update Script"
+echo -e " [\\e[36m•1\\e[0m] Panel Domain"
+echo -e " [\\e[36m•2\\e[0m] Speedtest VPS"
+echo -e " [\\e[36m•3\\e[0m] Set Auto Reboot"
+echo -e " [\\e[36m•4\\e[0m] Restart All Service"
+echo -e " [\\e[36m•5\\e[0m] Cek Bandwith"
+echo -e " [\\e[36m•6\\e[0m] Install TCP BBR"
+echo -e " [\\e[36m•7\\e[0m] DNS CHANGER"
+echo -e " [\\e[36m•8\\e[0m] Update Script"
+echo -e " [\\e[36m•9\\e[0m] Backup Config"
+echo -e " [\\e[36m•10\\e[0m] Restore Config"
 echo -e ""
-echo -e " [\e[31m•0\e[0m] \e[31mBACK TO MENU\033[0m"
+echo -e " [\\e[31m•0\\e[0m] \\e[31mBACK TO MENU\\033[0m"
 echo -e   ""
 echo -e   "Press x or [ Ctrl+C ] • To-Exit"
 echo -e   ""
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\\033[0m"
 echo -e ""
 read -p " Select menu : " opt
 echo -e ""
@@ -30,6 +32,8 @@ case $opt in
 6) clear ; m-tcp ; exit ;;
 7) clear ; m-dns ; exit ;;
 8) clear ; m-update ; exit ;;
+9) clear ; backup.sh ; exit ;;
+10) clear ; restore.sh ; exit ;;
 0) clear ; menu ; exit ;;
 x) exit ;;
 *) echo -e "" ; echo "Anda salah tekan" ; sleep 1 ; m-system ;;
